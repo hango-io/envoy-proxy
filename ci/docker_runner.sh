b84f8151ae0d58c008b9f6400e867c8ce3cccb24
tmsnan <877625878@qq.com>
2021-09-03 11:53:04 +0800

    fix:run envoy build container as a non-root user (#7)
    Co-authored-by: zhao nan <zhaonan06@corp.netease.com>

--- ci/docker_runner.sh
+++ ci/docker_runner.sh
@@ -28,7 +28,7 @@ docker run --rm ${DOCKER_RUNNING_MODE} --privileged ${DOCKER_TTY_OPTION} -e HTTP
     -e HTTPS_PROXY=${https_proxy} -v $(dirname ${SSH_AUTH_SOCK}):$(dirname ${SSH_AUTH_SOCK}) -v ${HOME}:${HOME} \
     -v "$PWD":/source -v ${ENVOY_DOCKER_BUILD_DIR}:/build -v /var/run/docker.sock:/var/run/docker.sock \
     -v /opt/envoy-cache:/opt/envoy-cache ${ADDITIONAL_VOLUME} -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
-    -v /etc/passwd:/etc/passwd -u root:root \
-    -e BAZEL_BUILD_EXTRA_OPTIONS -e BAZEL_EXTRA_TEST_OPTIONS -e BAZEL_REMOTE_CACHE -e BAZEL_REMOTE_INSTANCE \
-    -e NUM_CPUS -e ENVOY_RBE --cap-add SYS_PTRACE --cap-add NET_RAW --cap-add NET_ADMIN ${ENVOY_DOCKER_IMAGE} \
+    --user vscode -e BAZEL_BUILD_EXTRA_OPTIONS -e BAZEL_EXTRA_TEST_OPTIONS \
+    -e BAZEL_REMOTE_CACHE -e BAZEL_REMOTE_INSTANCE -e NUM_CPUS -e ENVOY_RBE \
+    --cap-add SYS_PTRACE --cap-add NET_RAW --cap-add NET_ADMIN ${ENVOY_DOCKER_IMAGE} \
     /bin/bash -c "${DOCKER_RUNNING_COMMAND}"
